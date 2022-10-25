b832049858d09970435633f84f3b726cccbcafed
diego <guatemaladiegojose@gmail.com>
2022-10-25 19:57:48 -0400

    commit tree 7738e3e3478c51080429600ea866f1bdd1744d9e

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+
+function appendCat(name){
+    const coppyCat = [...cats, name]    
+    return coppyCat
+    
+} 
+function prependCat(name){
+    const coppyCattwo = [name, ...cats];
+    return coppyCattwo
+}
+function removeLastCat(){
+    const coppythree = cats.slice(0, cats.length-1);
+    return coppythree;
+
+}
+function removeFirstCat(){
+    const coppyFour = cats.slice(1);
+    return coppyFour;
+
+
+}
